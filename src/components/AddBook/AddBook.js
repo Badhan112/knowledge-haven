5823e9cf136cdffdbafc1bd587d7b25e6b50ebd7
Badhan112 <badhancsesastc112@gmail.com>
2021-03-31 04:16:56 +0600

    add book input field handeled

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const AddBook = () => {
+    const [newBook, setNewBook] = useState({
+        name: '',
+        author: '',
+        price: '',
+        bookImgUrl: '',
+    });
+
+    const handleInputField = event => {
+        const bookInfo = {...newBook};
+        bookInfo[event.target.name] = event.target.value;
+        setNewBook(bookInfo);
+    }
+
+    const handleFormSubmit = event => {
+        console.log(newBook);
+        event.preventDefault();
+    }
     return (
         <div>
-            <h1>Add Book</h1>
+            <h3>Add Book</h3>
+            <Form onSubmit={handleFormSubmit}>
+                <Form.Group>
+                    <Form.Label>Book Name</Form.Label>
+                    <Form.Control name="name" onBlur={handleInputField} required type="text" placeholder="Enter Name" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Author Name</Form.Label>
+                    <Form.Control name="author" onBlur={handleInputField} required type="text" placeholder="Enter Name" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Add Price</Form.Label>
+                    <Form.Control name="price" onBlur={handleInputField} required type="number" placeholder="Enter Price" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.File label="Add Book Cover Photo" />
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
         </div>
     );
 };
