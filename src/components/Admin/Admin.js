7951dc7eb79c9696c0bd9144668c67649d958d58
Badhan112 <badhancsesastc112@gmail.com>
2021-03-31 02:31:29 +0600

    add Admin with material-ui vertical icon TAB

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,9 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import QueueIcon from '@material-ui/icons/Queue';
+import EditIcon from '@material-ui/icons/Edit';
+import ManageBooks from '../ManageBooks/ManageBooks';
+import AddBook from '../AddBook/AddBook';
+import EditBook from '../EditBook/EditBook';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: 224,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
+}));
 
 const Admin = () => {
+    const classes = useStyles();
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
-        <div>
-            <h1>Admin</h1>
+        <div className={classes.root}>
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                className={classes.tabs}
+            >
+                <Tab label={<div><DashboardIcon style={{marginRight: "10px"}} />Manage Books</div>} {...a11yProps(0)} />
+                <Tab label={<div><QueueIcon style={{marginRight: "10px"}} />Add Book</div>} {...a11yProps(1)} />
+                <Tab label={<div><EditIcon style={{marginRight: "10px"}} />Edit Book</div>} {...a11yProps(2)} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                <ManageBooks />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <AddBook />
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                <EditBook />
+            </TabPanel>
         </div>
     );
 };
