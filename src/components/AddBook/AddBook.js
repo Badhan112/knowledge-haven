eaccfce2871383a9b9488c3473da2683e08a51fd
Badhan112 <badhancsesastc112@gmail.com>
2021-03-31 14:22:45 +0600

    imgbb upload api added

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
@@ -8,15 +9,33 @@ const AddBook = () => {
         price: '',
         bookImgUrl: '',
     });
+    const [imageData, setImageData] = useState();
+
 
     const handleInputField = event => {
-        const bookInfo = {...newBook};
+        const bookInfo = { ...newBook };
         bookInfo[event.target.name] = event.target.value;
         setNewBook(bookInfo);
     }
 
+    const handleFileInput = event => {
+        const data = new FormData();
+        data.set('key', '617888b32f13454101eca1608e88c1dc');
+        data.append('image', event.target.files[0]);
+        setImageData(data);
+    }
+
     const handleFormSubmit = event => {
-        console.log(newBook);
+        axios.post('https://api.imgbb.com/1/upload', imageData)
+        .then(res => {
+            const info = { ...newBook };
+            console.log(res.data.data.display_url);
+            info.bookImgUrl = res.data.data.display_url;
+            setNewBook(info);
+        })
+        .catch(error => {
+            console.log(error);
+        });
         event.preventDefault();
     }
     return (
@@ -36,7 +55,7 @@ const AddBook = () => {
                     <Form.Control name="price" onBlur={handleInputField} required type="number" placeholder="Enter Price" />
                 </Form.Group>
                 <Form.Group>
-                    <Form.File label="Add Book Cover Photo" />
+                    <Form.File onChange={handleFileInput} required label="Add Book Cover Photo" />
                 </Form.Group>
                 <Button variant="primary" type="submit">Submit</Button>
             </Form>
